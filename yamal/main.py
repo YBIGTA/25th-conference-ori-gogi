ddf9bb762f6650f733747aa6d90b796c86b4428d
-3σ <yurippe@m3sigma.net>
2024-12-24 10:49:13 +0900

    Update main.py

--- yamal/main.py
+++ yamal/main.py
@@ -6,14 +6,10 @@ from prompt import prompt
 app = FastAPI()
 templates = Jinja2Templates(directory="static")
 
-@app.get("/", response_class=HTMLResponse)
-async def form_get(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
-@app.post("/number", response_class=HTMLResponse)
+@app.post("/number")
 async def submit_form(request: Request, name: str = Form(...)):
     ratio, answer, g_name = prompt(name)
-    return templates.TemplateResponse("number.html", {"request": request, "ratio": ratio, "answer": answer,"g_name":g_name})
+    return {"request": request, "ratio": ratio, "answer": answer,"g_name":g_name}
 
 if __name__ == "__main__":
     import uvicorn
